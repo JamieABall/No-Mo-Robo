9a71108ae904800b3c95eb881cf9535d745c1657
JamieABall <jamiegamer93@att.net>
2015-03-05 12:45:57 -0800

    Create Global.js

---
+++ Global.js
@@ -0,0 +1,13 @@
+/* Global.js
+ *******************************************************************************************************************************
+ An interface for ememy types
+ *******************************************************************************************************************************
+   @authors Jamsion Ball Graham Culver
+   @date 3/5/15
+   @last update 3/5/15
+   @last editor
+*/
+
+var Robot = {spriteSource: ROBOT_SPRITE_SRC, zIndex: robotZ, spriteWidth: robotWidth, spriteHight: robotHeight};
+var HeavyBot = (spriteSource: HEAVY_BOT_SRC, zIndex: heavyZ, spriteWidth: heavyWidth, spriteHeight: heavyHeight};
+var SpeedBot = {spriteSource: SPEED_BOT_SRC, zIndex: speedZ, spriteWidth: speedWidth, spriteHeight: speedHeight};
