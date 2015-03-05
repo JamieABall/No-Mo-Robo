cdb1fee155ea7921de6cfadbd26270b2e4b3cf3b
JamieABall <jamiegamer93@att.net>
2015-03-05 15:39:38 -0800

    Update Global.js

--- Global.js
+++ Global.js
@@ -14,12 +14,12 @@ var robotZ = 4;
 var robotWidth;
 var robotHeight;
 
-var HEAVY_BOT_SPRITE_SRC;
+var HEAVY_BOT_SPRITE_SRC = "http://i.imgur.com/L99co4m.png";
 var heavyZ = 4;
 var heavyWidth;
 var heavyHeight;
 
-var SPEED_BOT_SRC;
+var SPEED_BOT_SRC = "http://i.imgur.com/87MwSSj.png";
 var speedZ = 4;
 var speedWidth;
 var speedHeight;
