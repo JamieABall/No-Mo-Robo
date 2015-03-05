353272e4b877c6b6289a66b0b946ac0247c1bc7f
JamieABall <jamiegamer93@att.net>
2015-03-05 13:12:20 -0800

    Update Global.js

--- Global.js
+++ Global.js
@@ -7,7 +7,21 @@
    @last update 3/5/15
    @last editor
 */
+var ROBOT_SPRITE_SRC;
+var robotZ;
+var robotWidth;
+var robotHeight;
 
-var Robot = {spriteSource: ROBOT_SPRITE_SRC, zIndex: robotZ, spriteWidth: robotWidth, spriteHight: robotHeight};
+var HEAVY_BOT_SPRITE_SRC;
+var heavyZ;
+var heavyWidth;
+var heavyHeight;
+
+var SPEED_BOT_SRC;
+var speedZ;
+var speedWidth;
+var speedHeight;
+
+/*var Robot = {spriteSource: ROBOT_SPRITE_SRC, zIndex: robotZ, spriteWidth: robotWidth, spriteHight: robotHeight};
 var HeavyBot = (spriteSource: HEAVY_BOT_SRC, zIndex: heavyZ, spriteWidth: heavyWidth, spriteHeight: heavyHeight};
-var SpeedBot = {spriteSource: SPEED_BOT_SRC, zIndex: speedZ, spriteWidth: speedWidth, spriteHeight: speedHeight};
+var SpeedBot = {spriteSource: SPEED_BOT_SRC, zIndex: speedZ, spriteWidth: speedWidth, spriteHeight: speedHeight};*/
